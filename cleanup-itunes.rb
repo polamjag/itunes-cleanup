db91dbec6f0ce9d31c2056b1763f3bb2c2613758
polamjag <s@polamjag.info>
2016-04-04 05:31:13 +0900

    initial

---
+++ cleanup-itunes.rb
@@ -0,0 +1,45 @@
+require 'nokogiri'
+require 'uri'
+
+if ARGV.size != 2
+  puts <<-EOS
+usage: bundle exec cleanup-itunes.rb <path to iTunes Library.xml> <path to target iTunes Media path>
+
+example: bundle exec cleanup-itunes.rb "~/Music/iTunes/iTunes Library.xml" "~/Music/iTunes/iTunes Media/Music/"
+  EOS
+  exit 1
+end
+
+itl = open(ARGV[0]).read # iTunes Library.xml
+target_path = ARGV[1]
+
+doc = Nokogiri::XML itl
+
+itunes_medium = doc.xpath("/plist/dict/key[. = 'Tracks']/following-sibling::*[1]/dict/key[. = 'Location']/following-sibling::*[1]/text()").map do |i|
+  URI.decode URI.parse(i.text).path
+end
+
+puts "Detected #{itunes_medium.size} files from iTunes Library.xml"
+
+itunes_files = []
+Dir.chdir target_path do
+  itunes_files = Dir.glob("**/*.{mp3,m4a,m4v,wav,mp4,aac,aif,aiff,mov,mpg}")
+end
+
+puts "Detected #{itunes_files.size} files from iTunes Media directory"
+
+itunes_files.map! do |i|
+  i = File.join(File.expand_path(target_path), i)
+end
+
+delete_target = (itunes_files - itunes_medium)
+
+puts "Deleting #{delete_target.size} files ..."
+
+delete_target.each_with_index do |f, i|
+  puts "  -> delete (#{i}/#{delete_target.size}): #{f}"
+  File.delete f
+end
+
+puts "Done"
+
